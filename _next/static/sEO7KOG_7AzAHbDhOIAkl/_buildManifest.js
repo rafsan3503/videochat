d067a569c3fe5f4936c4913f29cc1a2cc09512eb
Md Mahmud Hasan <108387443+rafsan3503@users.noreply.github.com>
2023-11-09 12:39:47 +0600

    first commit

---
+++ _next/static/sEO7KOG_7AzAHbDhOIAkl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/230-d19dcd2a22da607a.js","static/chunks/pages/index-c8f74b12d39f4866.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
